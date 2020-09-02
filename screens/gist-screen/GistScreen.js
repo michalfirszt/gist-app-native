5df9951d67678db9aee61c07537452f8d69bc58a
michalfirszt <michalfiszu@gmail.com>
2020-09-02 20:42:56 +0200

    tab-icons

--- screens/gist-screen/GistScreen.js
+++ screens/gist-screen/GistScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from "@expo/vector-icons"
 import FilesScreen from "../files-screen/FilesScreen";
 import CommentsScreen from "../comments-screen/CommentsScreen";
 
@@ -9,8 +10,12 @@ function GistScreen({route, navigation}) {
 
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Files" children={() => <FilesScreen gist={gist} navigation={navigation} />} />
-            <Tab.Screen name="Comments" children={() => <CommentsScreen gist={gist} navigation={navigation} />} />
+            <Tab.Screen name="Files"
+                        children={() => <FilesScreen gist={gist} navigation={navigation} />}
+                        options={{ tabBarIcon: ({color, size}) => <MaterialIcons name="insert-drive-file" size={size} color={color} /> }} />
+            <Tab.Screen name="Comments"
+                        children={() => <CommentsScreen gist={gist} navigation={navigation} />}
+                        options={{ tabBarIcon: ({color, size}) => <MaterialIcons name="comment" size={size} color={color} /> }} />
         </Tab.Navigator>
     )
 }
